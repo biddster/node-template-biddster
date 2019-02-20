6600ffe785508875610647ac021e008a12b4b6ad
Luke <luke@utf11.com>
2019-02-20 21:59:35 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// TODO write me
+console.log('WRITE ME');
+
+module.exports = () => {
+    console.log('DO SOMETHING');
+};
---
+++ test/test.js
@@ -0,0 +1,8 @@
+const index = require('../index');
+
+describe('template-tests', function() {
+    it('should test something', function() {
+        // TODO write me
+        index();
+    });
+});
